db5dc21111880cbf241d1c8fda838d4aeba213ff
Enis Ahmet TÜRK <eaturk97@gmail.com>
2023-07-28 15:46:06 +0300

    .....

---
+++ App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>hello wwworld!</Text>
+      <Text>MERHABAYIN</Text>
+      <Text>MERHABAYIN</Text>
+      <Text>MERHABAYIN</Text>
+      <StatusBar style="cover" />
+      
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#8b658b',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
