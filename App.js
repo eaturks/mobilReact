b64388ae9b5831f3eda46fbe6f15bc4ec216c685
Enis Ahmet TÜRK <eaturk97@gmail.com>
2023-08-01 16:41:04 +0300

    son commit

--- App.js
+++ App.js
@@ -1,59 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Text, View } from 'react-native';
+import {styles}from './AppStyle';
 
 export default function App() {
 
 
   return (
     <View style={styles.container}>
-      <View style={styles.top}></View>
+      
+      <View style={styles.top}>
+      <Text style={styles.title}>eaTurk</Text> 
+      </View>
       <View style={styles.center}></View>
-      <View style={styles.bottom}></View>
-      
-     
-       
-       
-      
-      
-     
-
-      
-      <StatusBar style="cover" />
+      <View style={styles.bottom}></View>  
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    //width: '100%',
-    //height :'100%',
-    flex:1,
-    backgroundColor: '#97ffff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexDirection:'column'
-  },
-  top:{
-
-flex:4,
-width:'100%',
-backgroundColor:'blue',
-
-  },
-
-  center: {
-    
-    width:250,
-flex:1,
-backgroundColor:'purple',
-
-  },
-
-  bottom:{
-    flex:1,
-    width:'100%',
-    backgroundColor:'gray',
-  }
-
-
-})
---
+++ AppStyle.js
@@ -0,0 +1,54 @@
+import { StyleSheet } from "react-native";
+
+
+
+
+export const styles = StyleSheet.create({
+    container: {
+      //width: '100%',
+      //height :'100%',
+      flex:1,
+      backgroundColor: '#f0f',
+      alignItems: 'center',
+      justifyContent: 'center',
+      flexDirection:'column'
+    },
+  
+  
+    top:{
+  
+  flex:4,
+  width:'100%',
+  backgroundColor:'blue',
+  alignItems: 'center',
+  justifyContent: 'center',
+  
+    },
+  
+    center: {
+      
+      width:100,
+  flex:1,
+  backgroundColor:'purple',
+  
+    },
+  
+    bottom:{
+      flex:1,
+      width:'100%',
+      backgroundColor:'gray',
+    },
+  
+    title:{
+    
+      color:'white',
+      fontSize:50,
+      fontWeight:'bold',
+  
+  
+    }
+  
+  
+  })
+
+ 
